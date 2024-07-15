ff8455b2dd5d664076d02fc2a12cd5ceef2a611c
sigma <messouaboya17@gmail.com>
2024-07-15 15:44:42 +0900

    Users Services setup

---
+++ src/Procedures/users_procedure.py
@@ -0,0 +1,38 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 2024/07/15
+🚀 Welcome to the Awesome Python Script 🚀
+
+User: mesabo
+Email: mesabo18@gmail.com / messouaboya17@gmail.com
+Github: https://github.com/mesabo
+Univ: Hosei University
+Dept: Science and Engineering
+Lab: Prof YU Keping's Lab
+"""
+class UserProcedure:
+
+    def get_user_by_username_or_email_or_phone(self, username, email):
+        pass
+
+    def get_user_by_id(self, user_id):
+        pass
+
+    def get_all_users(self):
+        pass
+
+    def create_user(self):
+        pass
+
+    def update_user(self, ):
+        pass
+
+    def delete_user(self):
+        pass
+
+    def download_csv(self):
+        pass
+
+    def upload_csv(self):
+        pass
